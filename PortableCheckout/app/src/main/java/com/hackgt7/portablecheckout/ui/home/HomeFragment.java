0f85a8fde7c89445a64edb0d323e1911eba8da9a
Nick <nhutchison3@gatech.edu>
2020-10-18 00:44:10 -0400

    fixed scanner on dashboard

--- PortableCheckout/app/src/main/java/com/hackgt7/portablecheckout/ui/home/HomeFragment.java
+++ PortableCheckout/app/src/main/java/com/hackgt7/portablecheckout/ui/home/HomeFragment.java
@@ -1,31 +1,20 @@
 package com.hackgt7.portablecheckout.ui.home;
 
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.google.zxing.integration.android.IntentIntegrator;
-import com.google.zxing.integration.android.IntentResult;
-import com.hackgt7.portablecheckout.CaptureAct;
 import com.hackgt7.portablecheckout.R;
 
-public class HomeFragment extends Fragment implements View.OnClickListener {
-    Button scanButton;
-    int scannerOut = -1;
+public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
 
@@ -42,55 +31,11 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
             }
         });
 
-        this.scanButton = (Button) root.findViewById(R.id.scanBtn);
-        if (scanButton == null) {
-            Log.d("HOME", "scan btn is null");
-        }
-        scanButton.setOnClickListener(this::onClick);
         return root;
 
     }
 
-    /**
-     * Called when a view has been clicked.
-     *
-     * @param v The view that was clicked.
-     */
-    @Override
-    public void onClick(View v) {
-        Log.d("WHAT", "button firing");
-        barScan();
-    }
 
-    private void barScan() {
-        IntentIntegrator integrator = new IntentIntegrator(getActivity());
-        integrator.setCaptureActivity(CaptureAct.class);
-        integrator.setOrientationLocked(false);
-        integrator.setDesiredBarcodeFormats(IntentIntegrator.PRODUCT_CODE_TYPES);
-        integrator.setPrompt("Scanning Bar Code");
-        integrator.initiateScan();
-    }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
-        if (result != null) {
-            if (result.getContents() != null) {
-                this.scannerOut = resultCode;
-                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-                builder.setMessage(result.getContents());
-                builder.setTitle("Scanning Result");
-                builder.setPositiveButton("Scan Again",
-                        (DialogInterface dialog, int which) -> barScan())
-                        .setNegativeButton("finish",
-                                (DialogInterface dialog, int which) -> {});
-                AlertDialog dialog = builder.create();
-                dialog.show();
-            } else {
-                Toast.makeText(getContext(), "no results", Toast.LENGTH_SHORT);
-            }
-        } else {
-            super.onActivityResult(requestCode, resultCode, data);
-        }
-    }
+
 }
