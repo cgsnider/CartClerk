1e6f0cd3445ba09a0221656bfe09d65a0a4f8b74
Nick <nhutchison3@gatech.edu>
2020-10-17 22:12:37 -0400

    cart builds from API! wow!

--- PortableCheckout/app/src/main/java/com/hackgt7/portablecheckout/ui/notifications/NotificationsFragment.java
+++ PortableCheckout/app/src/main/java/com/hackgt7/portablecheckout/ui/notifications/NotificationsFragment.java
@@ -1,18 +1,18 @@
 package com.hackgt7.portablecheckout.ui.notifications;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.hackgt7.portablecheckout.R;
 
@@ -32,9 +32,22 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
-public class NotificationsFragment extends Fragment implements View.OnClickListener {
+public class NotificationsFragment extends Fragment {
 
-    private Button getCart;
+    private View root;
+
+    private ArrayList<String> nameList;
+    private ArrayAdapter<String> nameAdapter;
+    private ListView nameView;
+
+    private ArrayList<String> priceList;
+    private ArrayAdapter<String> priceAdapter;
+    private ListView priceView;
+
+    private ArrayList<String> quanList;
+    private ArrayAdapter<String> quanAdapter;
+    private ListView quanView;
+
     private NotificationsViewModel notificationsViewModel;
     private Date date;
     private ArrayList<CartItem> items;
@@ -81,18 +94,37 @@ public class NotificationsFragment extends Fragment implements View.OnClickListe
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        notificationsViewModel =
-                new ViewModelProvider(this).get(NotificationsViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_notifications, container, false);
-        final TextView textView = root.findViewById(R.id.text_notifications);
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        getCart = (Button) root.findViewById(R.id.get_cart_button);
-        getCart.setOnClickListener(this::onClick);
+
+        root = inflater.inflate(R.layout.fragment_notifications, container, false);
+        getAndBuildCartJSON();
+        nameList = new ArrayList<>();
+        nameView = root.findViewById(R.id.name_list);
+        nameAdapter = new ArrayAdapter<String>(this.getContext(), android.R.layout.simple_list_item_1);
+        nameView.setAdapter(nameAdapter);
+        setUpListViewListener(nameView);
+
+        priceList = new ArrayList<>();
+        priceView = root.findViewById(R.id.price_list);
+        priceAdapter = new ArrayAdapter<String>(this.getContext(), android.R.layout.simple_list_item_1);
+        priceView.setAdapter(priceAdapter);
+        setUpListViewListener(priceView);
+
+        quanList = new ArrayList<>();
+        quanView = root.findViewById(R.id.quantity_list);
+        quanAdapter = new ArrayAdapter<String>(this.getContext(), android.R.layout.simple_list_item_1);
+        quanView.setAdapter(quanAdapter);
+        setUpListViewListener(quanView);
+
+        for(int i = 0; i < items.size(); i++) {
+            this.addItem(this.getView(), "" + items.get(i).quantity, quanAdapter);
+        }
+        for(int i = 0; i < items.size(); i++) {
+            this.addItem(this.getView(), items.get(i).name, nameAdapter);
+        }
+        for(int i = 0; i < items.size(); i++) {
+            this.addItem(this.getView(), "" + items.get(i).price, priceAdapter);
+        }
+
         return root;
     }
 
@@ -104,13 +136,49 @@ public class NotificationsFragment extends Fragment implements View.OnClickListe
         return dateFormat.format(calendar.getTime()).concat(" GMT");
     }
 
-    /**
-     * Called when a view has been clicked.
-     *
-     * @param v The view that was clicked.
-     */
-    @Override
-    public void onClick(View v) {
+
+
+    private void setUpListViewListener(ListView lv) {
+        lv.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Context context = getContext();
+                Toast.makeText(context, "Item Removed", Toast.LENGTH_LONG).show();
+
+                nameAdapter.remove(nameList.get(i));
+                priceAdapter.remove(priceList.get(i));
+                quanAdapter.remove(quanList.get(i));
+
+                nameList.remove(i);
+                priceList.remove(i);
+                quanList.remove(i);
+
+                nameAdapter.notifyDataSetChanged();
+                priceAdapter.notifyDataSetChanged();
+                quanAdapter.notifyDataSetChanged();
+
+                return true;
+            }
+        });
+    }
+
+    private void addItem(View view, String str, ArrayAdapter adapter) {
+        if (!(str.equals(""))) {
+            adapter.add(str);
+            adapter.notifyDataSetChanged();
+        }
+    }
+//    /**
+//     * Called when a view has been clicked.
+//     *
+//     * @param v The view that was clicked.
+//     */
+//    @Override
+//    public void onClick(View v) {
+//
+//    }
+
+    public void getAndBuildCartJSON() {
         Response response = null;
         OkHttpClient client = new OkHttpClient().newBuilder()
                 .build();
@@ -118,10 +186,10 @@ public class NotificationsFragment extends Fragment implements View.OnClickListe
                 .url("https://gateway-staging.ncrcloud.com/emerald/selling-service/v1/carts/rdfvFVWJq0euHPrwO9PThQ/items")
                 .method("GET", null)
                 .addHeader("Content-Type", "application/json")
-                .addHeader("Authorization", "AccessKey 48ea19ed04ad4ae7811f57f307faed31:w+GnYk2URSDRm7ebT0G8Z824giJFDUslfhQMfE4zOkX8ulEy0mukMDbfjy4Y5VK78CoFBYChRFWnDq4it03maQ==")
+                .addHeader("Authorization", "AccessKey 48ea19ed04ad4ae7811f57f307faed31:6hkgiY8mJKNUk8ZziHArIX6jYrEdvjsSqOM55ME/PQ8IahsmP3gNwV/onLWbDQvP/w6FiCDzzFgvkkp3JdAc2A==")
                 .addHeader("nep-organization", "553888f84fdb4ce481dfea21e3202930")
                 .addHeader("nep-enterprise-unit", "fe2c3bf9a4194797a234159c1cd34797")
-                .addHeader("Date", "Sun, 18 Oct 2020 01:12:02 GMT")
+                .addHeader("Date", "Sun, 18 Oct 2020 01:56:30 GMT")
                 .build();
         Log.d("MAIN", getServerTime());
         Log.d("MAIN", "Sat, 17 Oct 2020 23:09:53 GMT");
